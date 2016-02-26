832f008d5ce0bbf0aef904064328d2a152c39174
jecheng <jenn.cheng89@gmail.com>
2016-02-25 23:44:02 -0800

    pictures

--- photo_project/script.js
+++ photo_project/script.js
@@ -1,11 +1,13 @@
-var elem = document.querySelector('.grid');
-var msnry = new Masonry(elem,{
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
+// 
+//
+// $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: 160
+// })
+var $grid = $('.grid').masonry({
+
 });
 
-// element argument can be a selector string for an individual element
-var msnry = new Masonry('.grid',{
-  // options
+$grid.imagesLoaded().progress(function(){
+  $grid.masonry('layout');
 });

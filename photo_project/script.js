d9cad1f188fe3028e3ab50b60ec5ba77120880c8
jecheng <jenn.cheng89@gmail.com>
2016-02-28 19:17:24 -0800

    photo project

--- photo_project/script.js
+++ photo_project/script.js
@@ -1,13 +1,15 @@
-// 
-//
-// $('.grid').masonry({
-//   itemSelector: '.grid-item',
-//   columnWidth: 160
-// })
-var $grid = $('.grid').masonry({
 
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 150,
+  gutter: 25
 });
 
-$grid.imagesLoaded().progress(function(){
-  $grid.masonry('layout');
-});
+new AnimOnScroll( document.getElementById( 'grid' ), {
+		minDuration : 0.4,
+		maxDuration : 0.7,
+		viewportFactor : 0.2
+	} );
